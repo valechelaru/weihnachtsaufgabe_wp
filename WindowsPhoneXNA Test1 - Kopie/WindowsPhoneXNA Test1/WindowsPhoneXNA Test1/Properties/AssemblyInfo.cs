af35c86c089f84ffa4a61bfe237d72c4c3d983f2
valechelaru <valechelaru@gmail.com>
2015-01-03 14:26:55 +0100

    Rumgespiele mit Test App

---
+++ WindowsPhoneXNA Test1 - Kopie/WindowsPhoneXNA Test1/WindowsPhoneXNA Test1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// Allgemeine Informationen über eine Assembly werden über den folgenden 
+// Satz von Attributen kontrolliert. Ändern Sie diese Attributwerte, um die mit einer Assembly
+// verbundenen Informationen zu ändern.
+[assembly: AssemblyTitle("WindowsPhoneXNA Test1")]
+[assembly: AssemblyProduct("WindowsPhoneXNA Test1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Bei Einstellung von ComVisible auf falsch sind die Typen in dieser Assembly für 
+// COM-Komponenten nicht sichtbar.  Wenn Sie von COM aus auf einen Typ in dieser Assembly 
+// zugreifen müssen, stellen Sie das Attribut ComVisible bei diesem Typ auf wahr ein. Nur Windows-
+// Assemblys unterstützen COM.
+[assembly: ComVisible(false)]
+
+// Auf Windows gilt die folgende GUID für die ID von typelib, wenn dieses
+// COM ausgesetzt ist. Auf anderen Plattformen identifiziert sie den
+// Titelspeichercontainer bei Bereitstellung dieser Assembly auf dem Gerät eindeutig.
+[assembly: Guid("b93a4ff1-b97a-4c0c-a1ea-7d2c06a40e63")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Größere Version
+//      Kleinere Version 
+//      Build-Nummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("de-DE")]
